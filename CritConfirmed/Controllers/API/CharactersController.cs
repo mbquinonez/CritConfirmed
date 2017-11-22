400b4003a33c4c4b6dbb3ca34e6edcde8c5326e7
mbquinonez <mbquinonez@gmail.com>
2017-11-21 18:31:12 -0800

    Add Controller for Characters

--- CritConfirmed/Controllers/API/CharactersController.cs
+++ CritConfirmed/Controllers/API/CharactersController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using CritConfirmed.Services;
+using CritConfirmed.Services.DTOs.Characters;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -11,36 +13,26 @@ namespace CritConfirmed.Controllers.API
     [Route("api/Characters")]
     public class CharactersController : Controller
     {
+        private readonly ICharacterService _characterService;
+
+        public CharactersController(ICharacterService characterService)
+        {
+            _characterService = characterService;
+        }
         // GET: api/values
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IActionResult Get()
         {
-            return new string[] { "value1", "value2" };
+            List<CharacterDTO> characters = _characterService.GetCharacters();
+            return new ObjectResult(characters) { StatusCode = 200 };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
-        public string Get(int id)
+        public IActionResult Get(int id)
         {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            CharacterDTO character = _characterService.GetCharacter(id);
+            return new ObjectResult(character) { StatusCode = 200 };
         }
     }
 }
